5f06350971013057df3f59971896c3a5435020d8
Naor Fahima <55282396+NaorFahima@users.noreply.github.com>
2021-11-06 11:31:13 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/mobilesecuritytask1/MainActivity.java
+++ app/src/main/java/com/example/mobilesecuritytask1/MainActivity.java
@@ -60,15 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     public int getBatteryLevel() {
         Intent batteryIntent = registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-
-        // Error checking that probably isn't needed but I added just in case.
-        if(level == -1 || scale == -1) {
-            return 50;
-        }
-
-        return (level / scale) * 100;
+        return batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
     }
 
     private int getBrightnessValue() {
